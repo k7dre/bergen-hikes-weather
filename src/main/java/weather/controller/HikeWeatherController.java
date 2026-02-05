1ed4de85cc1c4a879e692abfeed6cb49b7824f30
186489 <186489@stud.hvl.no>
2026-02-05 18:50:49 +0100

    Allow CORS for frontend

--- src/main/java/weather/controller/HikeWeatherController.java
+++ src/main/java/weather/controller/HikeWeatherController.java
@@ -2,11 +2,16 @@ package weather.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import weather.dto.MountainWeatherResponse;
 import weather.service.WeatherService;
+
+@CrossOrigin(
+		origins = "https://bergen-hikes-weather-1.onrender.com"
+)
 
 @RestController
 public class HikeWeatherController {
